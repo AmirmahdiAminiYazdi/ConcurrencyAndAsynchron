b6a20b140f5503301ad4e4e80e684de5bc237460
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2025-01-05 11:05:55 +0330

    Add project files.

---
+++ ConcurrencyAndAsynchron.ThreadSample/CharPrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyAndAsynchron.ThreadSample
+{
+    public class CharPrinter
+    {
+        public void printStar()
+        {
+            for (int i = 0; i < 1000; i++) { Console.Write("*"); }
+        }
+        public void PrintDash() { for (int i = 0; i < 1000; i++) { Console.Write("-"); } }
+    }
+}
---
+++ ConcurrencyAndAsynchron.ThreadSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConcurrencyAndAsynchron.ThreadSample;
+
+CharPrinter charPrinter = new CharPrinter();
+Thread dashPrinterWorker = new Thread(charPrinter.PrintDash);
+dashPrinterWorker.Name = "dashPrinterWorker";
+Console.WriteLine($"{dashPrinterWorker.Name} is alive before start: {dashPrinterWorker.IsAlive}");
+
+dashPrinterWorker.Start();
+
+Console.WriteLine($"{dashPrinterWorker.Name} is alive after start: {dashPrinterWorker.IsAlive}");
+
+charPrinter.printStar();
+
+Console.WriteLine($"{dashPrinterWorker.Name} is alive after print STAR: {dashPrinterWorker.IsAlive}");
+Console.ReadLine();
+Console.WriteLine($"{dashPrinterWorker.Name} is alive after readline: {dashPrinterWorker.IsAlive}");
+
