f1b65672a3a54416e7cf595644efc7fc35615190
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2025-02-16 11:58:34 +0330

    initial CancellationToken

--- ConcurrencyAndAsynchron.AsyncSample/Program.cs
+++ ConcurrencyAndAsynchron.AsyncSample/Program.cs
@@ -1,5 +1,27 @@
 ﻿using ConcurrencyAndAsynchron.AsyncSample;
 
-var PrimeNumberSample = new PrimeNumberSample();
-PrimeNumberSample.DisplayPrimeNumber();
-Console.ReadLine();
+//var PrimeNumberSample = new PrimeNumberSample();
+//PrimeNumberSample.DisplayPrimeNumber();
+//Console.ReadLine();
+CancellationTokenSource cts = new CancellationTokenSource();
+var sample02 = new Sample02();
+
+var task = sample02.PrintAfter10Sec("My Message", cts.Token);
+Console.WriteLine("If you want to cancel the task press c");
+var cancelResult = Console.ReadLine();
+if(cancelResult == "c")
+{
+    cts.Cancel();
+}
+try
+{
+    await task;
+}
+catch (TaskCanceledException ex)
+{
+
+    var status = task.Status;
+    var isCanceled = task.IsCanceled;
+    Console.WriteLine($"Task is canceled : {isCanceled} & task is status : {status} .");
+    Console.WriteLine(ex.Message);
+}
---
+++ ConcurrencyAndAsynchron.AsyncSample/Sample02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyAndAsynchron.AsyncSample
+{
+    public class Sample02
+    {
+        public async Task PrintAfter10Sec(string message,CancellationToken cancellationToken)
+        {
+            await Task.Delay(10000,cancellationToken);
+            Console.WriteLine(message);
+        }
+    }
+}
